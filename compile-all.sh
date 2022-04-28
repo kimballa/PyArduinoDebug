bf4261e25d5e292e0b46f71d20df379ea5359cca
Aaron Kimball <akimball83@gmail.com>
2022-04-28 14:09:45 -0700

    Add helper script to compile all targets.

---
+++ compile-all.sh
@@ -0,0 +1,16 @@
+# (c) Copyright 2022 Aaron Kimball
+#
+# Compile PyArduinoDebug library for all supported architectures.
+
+read -r -d '' BOARDS << EOL
+arduino:avr:leonardo
+adafruit:samd:adafruit_feather_m4
+EOL
+
+set -e
+for board in ${BOARDS}; do
+  echo -e "\033[1m*** Compiling for Arduino architecture: ${board}\033[0m"
+  BOARD=${board} make clean install
+  echo ""
+  echo ""
+done
